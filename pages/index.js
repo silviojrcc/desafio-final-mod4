440144a163fba2cf34fcf0789de6f02819067c04
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-02-28 14:13:09 -0300

    saco comentarios de index.js

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,7 @@ function main() {
   const footerContainer = document.querySelector(".footer-container");
   footer(footerContainer);
   const contactMeContainer = document.querySelector(".contact-me-container");
-  
   contactMe(contactMeContainer);
-  const cardContainers = document.querySelectorAll(".my-services__card-container");
-  
-  // for (const cardContainer of cardContainers) {
-  //   card(cardContainer)
-  // }
-
 }
 
 main();
