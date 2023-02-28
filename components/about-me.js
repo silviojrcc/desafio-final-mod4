bae63b75910e1cfb2bcffde3b7e1f4abe8522258
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-02-28 19:31:37 -0300

    Arreglando about me

--- components/about-me.js
+++ components/about-me.js
@@ -22,16 +22,21 @@ function getAuthor() {
 }
 
 async function aboutMe(el) {
-  const aboutMeEl = document.createElement("div");
+  const aboutMeText = document.createElement("div");
+  const aboutMeImg = document.createElement("div");
   const author = await getAuthor();
 
-  aboutMeEl.innerHTML = `
+  aboutMeText.innerHTML = `
     <div class="about-me__text-container">
         <h2 class="about-me__title">Soy ${author.name}</h2>
         <p class="about-me__description">${author.description}</p>
     </div>
-    <img src="${author.image}" alt="" class="about-me__img">
     `;
 
-    el.appendChild(aboutMeEl);
+    aboutMeImg.innerHTML = `
+    <img src="${author.image}" alt="" class="about-me__img">
+    `
+
+    el.appendChild(aboutMeText);
+    el.appendChild(aboutMeImg);
 }
