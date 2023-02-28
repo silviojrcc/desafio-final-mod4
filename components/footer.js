5a411ef36375d7a08c44b79d5927dbcf9c499c97
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-02-28 14:37:37 -0300

    arreglando footer

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ function footer(el){
     const footerEl = document.createElement("div");
     footerEl.innerHTML = `
     <footer class="footer">
-        <div class="footer__logo"><h3>Mi logo</h3></div>
+        <div class="footer__logo"><a href=""><h3>Mi logo</h3></a></div>
         <div class="footer__links">
             <a href="./index.html" class="footer__link"><span class="footer_link-ico"><i class="fa-solid fa-house"></i></span>Home</a>
             <a href="./my-services.html" class="footer__link"><span class="footer_link-ico"><i class="fa-regular fa-user"></i></span>Servicios</a>
